11b7cc2a4d56e4d5be37ec265254025205db81f1
Fabio Jordao <fabiosjordao@gmail.com>
2021-10-10 23:19:21 -0300

    Configurando Graphql com express

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+import resolvers from './resolvers';
+import typeDefs from './schemas';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
+
+// app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
 
 app.listen(3333);
