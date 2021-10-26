a19be0c695358baa039be9eb239d9234e8ba814a
Fabio Jordao <fabiosjordao@gmail.com>
2021-10-26 08:42:24 -0300

    implementando routes

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import resolvers from './resolvers';
-import typeDefs from './schemas';
 import './database';
+import router from './routes';
 
 const app = express();
-
-const schema = makeExecutableSchema({
-  resolvers,
-  typeDefs,
-});
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  }),
-);
-
-// app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+app.use(express.json());
+app.use(router);
 
 app.listen(3333);
