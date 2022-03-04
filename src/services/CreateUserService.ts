8e7b863f2e0357083f5b0a3e22da0f9b9d21b70e
Fabio Jordao <fabiosjordao@gmail.com>
2022-03-04 00:26:44 -0300

    Config alias

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,5 @@
 import { getCustomRepository } from 'typeorm';
-import UserRepository from '../repositories/UserRepository';
+import UserRepository from '@repositories/UserRepository';
 
 interface IUserRequest {
   name: string;
