b301fa5e861ea75e35eacd4e861d6c19f5077ec1
Fabio Jordao <fabiosjordao@gmail.com>
2022-03-07 08:14:50 -0300

    Configurando injeção de dependência

--- src/services/ListUserService.ts
+++ src/services/ListUserService.ts
@@ -1,7 +1,9 @@
 import { getCustomRepository } from 'typeorm';
 import UsersRepositories from '@repositories/UserRepository';
 import { classToPlain } from 'class-transformer';
+import { injectable } from 'tsyringe';
 
+@injectable()
 class ListUserService {
   async execute() {
     const usersRepositories = getCustomRepository(UsersRepositories);
