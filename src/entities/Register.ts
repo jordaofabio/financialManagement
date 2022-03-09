7bd52525ea39a5fe54450a6a86ef9b6dc06a968f
Fabio Jordao <fabiosjordao@gmail.com>
2022-03-09 01:39:50 -0300

    Criando register

---
+++ src/entities/Register.ts
@@ -0,0 +1,37 @@
+import {
+  Entity, PrimaryColumn, CreateDateColumn, UpdateDateColumn, Column,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity()
+export class Register {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  value: number;
+
+  @Column()
+  type: number;
+
+  @Column()
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
