c23525df052d876eef38e9ceed4782244fc73600
Fabio Jordao <fabiosjordao@gmail.com>
2022-03-11 02:11:42 -0300

    Criando registro com relacionamento de tags

--- src/services/CreateRegisterService.ts
+++ src/services/CreateRegisterService.ts
@@ -1,20 +1,48 @@
 import { injectable } from 'tsyringe';
 import { getCustomRepository } from 'typeorm';
-import RegisterRepositoy from '@repositories/RegisterRepository';
+import RegisterRepository from '@repositories/RegisterRepository';
+import RegisterTagRepository from '@repositories/RegisterTagRepository';
+import Tag from '@/entities/Tag';
+import TagsRepositoy from '@/repositories/TagsRepository';
 
 interface IRegisterRequest {
-  name: string;
-  category_id: string;
-  description?: string;
-  value: number;
-  type: number;
-  date: Date;
+  name: string
+  category_id: string
+  description?: string
+  tags: Tag[],
+  value: number
+  type: number
+  date: Date
 }
+
+// interface IResgisterTag {
+//   tags: [string],
+//   register_id: string
+// }
 @injectable()
 class CreateRegisterService {
-  async execute({ name, category_id, description, value, type, date }: IRegisterRequest) {
+  // async createRegisterTag({ tags, register_id }: IResgisterTag) {
+  //   const registerTagrRepository = getCustomRepository(RegisterTagRepository);
+
+
+
+  //   const registerTagrAlredyExists = await registerTagrRepository.findOne({ tag_id, register_id });
+
+  //   if (!registerTagrAlredyExists) {
+  //     const registerTag = registerTagrRepository.create({ tag_id, register_id });
+  //     await registerTagrRepository.save(registerTag);
+  //   }
+
+    
+
+  // }
+
+  async execute({ name, tags, category_id, description, value, type, date }: IRegisterRequest) {
+    console.log('>>>>>>>>>>>>>>> tags', tags)
     try {
-      const registerRepositoy = getCustomRepository(RegisterRepositoy);
+      const registerRepository = getCustomRepository(RegisterRepository);
+      const tagRepository = getCustomRepository(TagsRepositoy);
+
       if (!name) {
         throw new Error('Invalid name!');
       }
@@ -26,10 +54,19 @@ class CreateRegisterService {
       if (!value || value < 0) {
         throw new Error('Invalid value!');
       }
+      const _tags = []
 
-      const register = registerRepositoy.create({ name, category_id, description, value, type, date: new Date(date) });
+      for await (const id of tags) {
+      console.log('>>>>>>>>>>>>>>> id', id)
+      const [tag] = await tagRepository.findByIds([id])
+        _tags.push(tag)
+      }
+      console.log('>>>>>>>>>>>>>>> _tags', _tags)
 
-      await registerRepositoy.save(register);
+
+      const register = registerRepository.create({ name, tags: _tags, category_id, description, value, type, date: new Date(date) });
+
+      await registerRepository.save(register);
 
       return register;
     } catch (err) {
